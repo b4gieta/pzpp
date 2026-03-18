eda6965ef81f187e86b83b6020b662095ddc07d1
Marcel Segieta <segieta.marcel@gmail.com>
2026-03-18 20:10:39 +0100

    Added proper views and controllers

---
+++ Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pzpp.Data;
+using pzpp.Models;
+
+public class PostController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public PostController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult Create(int threadId, string content)
+    {
+
+        if (string.IsNullOrWhiteSpace(content))
+            return RedirectToAction("Index", "Thread", new { id = threadId });
+
+        var post = new Post
+        {
+            ThreadId = threadId,
+            Content = content,
+            CreatedAt = DateTime.UtcNow,
+            UserId = 1
+        };
+
+        _context.Posts.Add(post);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index", "Thread", new { id = threadId });
+    }
+}
---
+++ Controllers/ThreadController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pzpp.Data;
+using pzpp.Models;
+
+public class ThreadController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ThreadController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index(int? id)
+    {
+        if (id == null)
+        {
+            // brak id - lista wątków
+            var threads = _context.Threads.ToList();
+            return View("List", threads);
+        }
+
+        // id podane - wyświetl wątek
+        var thread = _context.Threads
+            .Include(t => t.Posts)
+            .FirstOrDefault(t => t.Id == id.Value);
+
+        if (thread == null) return NotFound();
+
+        return View(thread);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateWithPost(string title, string content)
+    {
+        var user = _context.Users.FirstOrDefault();
+
+        if (user == null)
+        {
+            user = new User { Id = 1 };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        var thread = new pzpp.Models.Thread
+        {
+            Title = title,
+            CreatedAt = DateTime.UtcNow,
+            CreatedByUserId = user.Id
+        };
+
+        _context.Threads.Add(thread);
+        _context.SaveChanges();
+
+        var post = new Post
+        {
+            ThreadId = thread.Id,
+            Content = content,
+            CreatedAt = DateTime.UtcNow,
+            UserId = user.Id
+        };
+
+        _context.Posts.Add(post);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index", new { id = thread.Id });
+    }
+}
