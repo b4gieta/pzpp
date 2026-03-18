364d91b4c1a3fb27b08182fa06b7c27d1972f26e
Patryk <patrykpaw06@wp.pl>
2026-03-18 20:37:43 +0100

    Added:   -Controller:      |-AuthController.cs  -Services:      |-UserService.cs -Ajax     |-Script (at the bottom of _Layout.cshtml) -Fixed account panel when logged in -JSON with Hashed Password:     |-user.json

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using pzpp.Data;
+using pzpp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// DODAJ SERVICE
+builder.Services.AddScoped<UserService>();
+
+// SESSION (do logowania)
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(1);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -20,6 +32,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
---
+++ Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using pzpp.Models;
+namespace pzpp.Services
+{
+    public class UserService
+    {
+        private readonly string filePath = "users.json";
+
+        public List<User> GetAll()
+        {
+            if (!File.Exists(filePath))
+                return new List<User>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        }
+
+        public void SaveAll(List<User> users)
+        {
+            var json = JsonSerializer.Serialize(users, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(filePath, json);
+        }
+
+        public User? GetByLogin(string login)
+        {
+            return GetAll().FirstOrDefault(u => u.Login == login);
+        }
+
+        public bool UserExists(string login)
+        {
+            return GetAll().Any(u => u.Login == login);
+        }
+
+        public void Add(User user)
+        {
+            var users = GetAll();
+            users.Add(user);
+            SaveAll(users);
+        }
+    }
+}
